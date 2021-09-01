c818a0d99502de8fcfdb5ff351dced66ebb5fbe4
MAHAARUNCHALAM <71120158+MAHAARUNACHALAM@users.noreply.github.com>
2021-09-01 16:49:16 +0530

    BAck to Middle Cropping

--- app.py
+++ app.py
@@ -101,17 +101,17 @@ def predict():
 
         img=np.asarray(img)
 
-        crop1=img[0:100,0:380]
+        crop=img[280:320,55:380]
         Oimg=[]
 
-        Oimg.append(crop1[0:crop1.shape[1],0:55])
-        Oimg.append(crop1[0:crop1.shape[1],55:95])
-        Oimg.append(crop1[0:crop1.shape[1],95:140])
-        Oimg.append(crop1[0:crop1.shape[1],140:180])
-        Oimg.append(crop1[0:crop1.shape[1],180:230])
-        Oimg.append(crop1[0:crop1.shape[1],230:280])
-        Oimg.append(crop1[0:crop1.shape[1],280:320])
-        Oimg.append(crop1[0:crop1.shape[1],320:370])
+        Oimg.append(crop[0:crop.shape[1],0:40])
+        Oimg.append(crop[0:crop.shape[1],40:80])
+        Oimg.append(crop[0:crop.shape[1],80:110])
+        Oimg.append(crop[0:crop.shape[1],120:150])
+        Oimg.append(crop[0:crop.shape[1],150:190])
+        Oimg.append(crop[0:crop.shape[1],190:230])
+        Oimg.append(crop[0:crop.shape[1],230:270])
+        Oimg.append(crop[0:crop.shape[1],270:310])
         output1=[]
         for i in Oimg:
                 X = increase_brightness(i, value=80)
